fd0548f8ad1b62fb82f7b1e8fd3604cf67d29e32
Rafael Paiva <92996217+RafaelPaivex@users.noreply.github.com>
2023-01-23 12:08:57 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className='mt-auto text-center'>
             <p>Made by: Rafael Paiva</p>
-            <p><a href="https://github.com/RafaelPaivex">Github</a></p>
+            <p><a href="https://github.com/RafaelPaivex/GuessTheColorPRT">Github</a></p>
         </footer>
     );
 }
